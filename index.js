d3a34061bbe4887bfb77c53edaafef6c478a8262
Quách Tấn Dũng <dungquach12.com@gmail.com>
2024-12-18 08:14:33 +0700

    change index.html to login, up layout.hbs and index.js

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const expressHbs = require("express-handlebars");
+const { createPagination } = require("express-handlebars-paginate")
+
+// Configure static web folders
+app.use(express.static(__dirname + "/html"));
+
+// Configure view templates
+app.engine('hbs', expressHbs.engine({
+    layoutsDir: __dirname + "/views/layouts",
+    partialDir: __dirname + "/views/partials",
+    extname: "hbs",
+    defaultLayout: "layout",
+    runtimeOptions: {
+        allowProtoPropertiesByDefault: true,
+    },
+    helpers: {
+        createPagination,
+    }
+}));
+
+app.set("view engine", "hbs");
+
+// Routes
+// YOUR CODE HERE
+app.get("/", (req, res) =>
+    res.redirect("/thread"),
+);
+
+app.get("/thread", (req, res) =>
+    res.render("thread")
+);
+
+app.listen(port, () =>
+     console.log(`Example app listening on port ${port}`)
+);
