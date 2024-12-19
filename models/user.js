2dd1c8b451befe05658ebe004e152c52edad1fcb
Quách Tấn Dũng <dungquach12.com@gmail.com>
2024-12-19 10:03:07 +0700

    fix DB model, make username unique, and validate email field, also check null for username and pass

--- models/user.js
+++ models/user.js
@@ -30,11 +30,28 @@ module.exports = (sequelize, DataTypes) => {
       user_id: {
         type: DataTypes.UUID,
         primaryKey: true,
-        defaultValue: DataTypes.UUIDV4
+        defaultValue: DataTypes.UUIDV4,
       },
-      username: DataTypes.STRING,
-      email: DataTypes.STRING,
-      password_hash: DataTypes.STRING,
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false, // Ensure username is required
+        unique: true,     // Ensure uniqueness of username
+        validate: {
+          notEmpty: true, // Prevent empty strings
+        },
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true, // Ensure email is unique
+        validate: {
+          isEmail: true, // Validate email format
+        },
+      },
+      password_hash: {
+        type: DataTypes.STRING,
+        allowNull: false, // Password is required
+      },
       bio: DataTypes.TEXT,
       profile_picture: DataTypes.STRING,
     },
